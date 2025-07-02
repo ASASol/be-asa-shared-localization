a9ced2e9626607f67386cd93225ea2be70970f44
Huy Phạm <153051275+Huycute1104@users.noreply.github.com>
2025-07-02 11:45:42 +0700

    fix custom results

---
+++ be_asa_shared_api_base/Controllers/BaseLocalizedController.cs
@@ -0,0 +1,23 @@
+﻿using be_asa_shared_localization.Localization;
+using CoreApiResponse;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace be_asa_shared_api_base.Controllers
+{
+    public class BaseLocalizedController : BaseController
+    {
+        private readonly IJsonLocalizationService _localizer;
+
+        public BaseLocalizedController(IJsonLocalizationService localizer)
+        {
+            _localizer = localizer;
+        }
+
+        protected IActionResult CustomLocalizedResult(string key, object? data = null, HttpStatusCode code = HttpStatusCode.OK)
+        {
+            var message = _localizer[key];
+            return CustomResult(message, data, code);
+        }
+    }
+}
