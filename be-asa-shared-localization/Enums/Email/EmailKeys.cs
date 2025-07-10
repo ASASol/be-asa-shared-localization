e0107033098325bd0523100a54e54a9ed2cd9b2e
Huy Phạm <153051275+Huycute1104@users.noreply.github.com>
2025-07-10 10:07:02 +0700

    add enum email

---
+++ be-asa-shared-localization/Enums/Email/EmailKeys.cs
@@ -0,0 +1,10 @@
+﻿namespace be_asa_shared_localization.Enums.Email
+{
+    public enum EmailKeys
+    {
+        EMAIL_CONFIG_NOT_FOUND,
+        EMAIL_TEMPLATE_NOT_FOUND,
+        EMAIL_SEND_SUCCESS,
+        EMAIL_SEND_FAILED
+    }
+}
