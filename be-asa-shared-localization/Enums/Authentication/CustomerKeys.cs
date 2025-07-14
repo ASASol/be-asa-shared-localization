15056a03611b2c31f2ab6a748c159b78afd0bdff
Huy Phạm <153051275+Huycute1104@users.noreply.github.com>
2025-07-14 14:19:43 +0700

    fix enum

---
+++ be-asa-shared-localization/Enums/Authentication/CustomerKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace be_asa_shared_localization.Enums.Authentication
+{
+    public enum CustomerKeys
+    {
+        BRANCH_SETTING_VALUE_REQUIRED,
+        BRANCH_SETTING_INVALID_BOOL,
+        BRANCH_SETTING_INVALID_INT,
+        BRANCH_SETTING_INVALID_DATETIME,
+        BRANCH_SETTING_INVALID_ALLOWED
+    }
+}
