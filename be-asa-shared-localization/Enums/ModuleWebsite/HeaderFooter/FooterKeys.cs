1ede7de4a310916c9908ca368f1ff2d22b9788e9
Huy Thắng <huythang1306@gmail.com>
2025-08-15 11:52:19 +0700

    feat:feat key and vi,en

---
+++ be-asa-shared-localization/Enums/ModuleWebsite/HeaderFooter/FooterKeys.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace be_asa_shared_localization.Enums.ModuleWebsite.HeaderFooter
+{
+    public enum FooterKeys
+    {
+        NOT_FOUND,
+        COMPANY_NOT_FOUND,
+        INTERNAL_ERROR,
+        BUILD_TREE_ERROR,
+        INVALID_PAGE_INDEX,
+        INVALID_PAGE_SIZE,
+        INVALID_PARENT_ID,
+        INVALID_FOOTER_ID,
+        INVALID_REQUEST,
+        NAME_REQUIRED,
+        FOOTER_CODE_REQUIRED,
+        FOOTER_CODE_EXISTS,
+        UNAUTHORIZED,
+        CREATE_ERROR,
+        UPDATE_ERROR,
+        DELETE_ERROR,
+        HAS_CHILDREN_CANNOT_DELETE
+    }
+}
