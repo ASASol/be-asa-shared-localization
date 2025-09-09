4745cdc7583fe27304967839c00a2e50a567f06d
Huy Phạm <huypt@namviets.com>
2025-09-09 14:53:46 +0700

    update

--- be-asa-shared-localization/Enums/ModuleWebsite/SectionComponent/Blog/TagKeys.cs
+++ be-asa-shared-localization/Enums/ModuleWebsite/SectionComponent/Blog/TagKeys.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace be_asa_shared_localization.Enums.ModuleWebsite.SectionComponent.Blog
+﻿namespace be_asa_shared_localization.Enums.ModuleWebsite.SectionComponent.Blog
 {
     public enum TagKeys
     {
@@ -19,6 +13,8 @@ namespace be_asa_shared_localization.Enums.ModuleWebsite.SectionComponent.Blog
         UPDATE_FAILED,
         DELETE_FAILED,
         CONCURRENCY_ERROR,
-        INTERNAL_SERVER_ERROR
+        INTERNAL_SERVER_ERROR,
+        INVALID_FORMAT,
+        DUPLICATE
     }
 }
