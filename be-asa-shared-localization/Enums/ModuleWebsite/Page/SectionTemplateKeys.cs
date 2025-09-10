b61e37068a756a3af6471c12f8262a820c7b8b9b
Huy Phạm <huypt@namviets.com>
2025-09-10 10:02:31 +0700

    update

---
+++ be-asa-shared-localization/Enums/ModuleWebsite/Page/SectionTemplateKeys.cs
@@ -0,0 +1,10 @@
+﻿namespace be_asa_shared_localization.Enums.ModuleWebsite.Page
+{
+    public enum SectionTemplateKeys
+    {
+        SECTION_TEMPLATE_TYPE_NOT_FOUND,
+        SECTION_TEMPLATE_NOT_FOUND,
+        SECTION_TEMPLATE_CODE_EXISTS,
+        SECTION_TEMPLATE_DELETED_SUCCESS,
+    }
+}
