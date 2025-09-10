1625946121720cfffdf30a69f5cfc12497ac94b6
Huy Phạm <huypt@namviets.com>
2025-09-10 10:12:23 +0700

    update

--- be-asa-shared-localization/Enums/ModuleWebsite/Page/SectionKeys.cs
+++ be-asa-shared-localization/Enums/ModuleWebsite/Page/SectionKeys.cs
@@ -2,5 +2,12 @@
 {
     public enum SectionKeys
     {
+        SECTION_NOT_FOUND,
+        SECTION_CREATE_SUCCESS,
+        SECTION_UPDATE_SUCCESS,
+        SECTION_DELETE_SUCCESS,
+        SECTION_CREATE_FAILED,
+        SECTION_UPDATE_FAILED,
+        SECTION_DELETE_FAILED
     }
 }
