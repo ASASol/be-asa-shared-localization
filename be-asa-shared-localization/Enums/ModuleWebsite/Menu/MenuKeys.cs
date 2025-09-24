8c1638f60d578eaf79f7f93d80541d928b9bb47f
Huy Phạm <huypt@namviets.com>
2025-09-24 15:44:30 +0700

    fix vi en

--- be-asa-shared-localization/Enums/ModuleWebsite/Menu/MenuKeys.cs
+++ be-asa-shared-localization/Enums/ModuleWebsite/Menu/MenuKeys.cs
@@ -8,6 +8,7 @@
         PageNotPublished,
         ParentNotFound,
         ParentTypeMismatch,
-        ParentCircularReference
+        ParentCircularReference,
+        CodeAlreadyExists
     }
 }
