a311d49824f13572e1a54c3113a607d5e7c0ac89
Huy Phạm <huypt@namviets.com>
2025-10-02 11:14:22 +0700

    add vi en faq + diagram

---
+++ be-asa-shared-localization/Enums/ModuleWebsite/SectionComponent/Diagram/DiagramKeys.cs
@@ -0,0 +1,7 @@
+﻿namespace be_asa_shared_localization.Enums.ModuleWebsite.SectionComponent.Diagram
+{
+    public enum DiagramKeys
+    {
+        NOT_FOUND
+    }
+}
---
+++ be-asa-shared-localization/Enums/ModuleWebsite/SectionComponent/Faq/FaqKeys.cs
@@ -0,0 +1,7 @@
+﻿namespace be_asa_shared_localization.Enums.ModuleWebsite.SectionComponent.Faq
+{
+    public enum FaqKeys
+    {
+        NOT_FOUND
+    }
+}
