59053f95fa5480fd75823122ee668b3cfea287c4
Huy Phạm <huypt@namviets.com>
2025-10-10 00:33:13 +0700

    fix vi en

---
+++ be-asa-shared-localization/Enums/ModuleWebsite/Page/ArticleCategoryKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace be_asa_shared_localization.Enums.ModuleWebsite.Page
+{
+    public enum ArticleCategoryKeys
+    {
+        NOT_FOUND,                     // Khi không tìm thấy danh mục
+        NAME_ALREADY_EXISTS,           // Khi tên danh mục đã tồn tại
+        SUBCATEGORY_IN_USE_CANNOT_DELETE // Khi còn subcategory nên không xóa được
+    }
+}
