0fc7f4e35d008e77550080b2cf55db06ed0e1651
Huy Phạm <huypt@namviets.com>
2025-10-10 13:41:31 +0700

    fix enum

--- be-asa-shared-localization/Enums/ModuleWebsite/Page/ArticleCategoryKeys.cs
+++ be-asa-shared-localization/Enums/ModuleWebsite/Page/ArticleCategoryKeys.cs
@@ -4,6 +4,9 @@
     {
         NOT_FOUND,                     // Khi không tìm thấy danh mục
         NAME_ALREADY_EXISTS,           // Khi tên danh mục đã tồn tại
-        SUBCATEGORY_IN_USE_CANNOT_DELETE // Khi còn subcategory nên không xóa được
+        CATEGORY_IN_USE_CANNOT_DELETE, // Khi còn category nên không xóa được
+        PARENT_CANNOT_BE_SELF,          // Parent trùng chính nó
+        PARENT_NOT_FOUND,               // ParentId không tồn tại
+        PARENT_CIRCULAR_REFERENCE       // Vòng lặp đa cấp
     }
 }
--- be-asa-shared-localization/Enums/ModuleWebsite/Page/ArticleSubCategoryKeys.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace be_asa_shared_localization.Enums.ModuleWebsite.Page
-{
-    public enum ArticleSubCategoryKeys
-    {
-        NOT_FOUND,
-        NAME_ALREADY_EXISTS,
-        SUBCATEGORY_IN_USE_CANNOT_DELETE
-    }
-}
