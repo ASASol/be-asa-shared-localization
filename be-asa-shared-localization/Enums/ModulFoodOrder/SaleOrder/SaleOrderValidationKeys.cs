15e57ca6ab09289221020020bb45cef844cb63e8
Huy Phạm <huypt@namviets.com>
2026-02-06 15:19:49 +0700

    update loc

--- be-asa-shared-localization/Enums/ModulFoodOrder/SaleOrder/SaleOrderValidationKeys.cs
+++ be-asa-shared-localization/Enums/ModulFoodOrder/SaleOrder/SaleOrderValidationKeys.cs
@@ -27,5 +27,6 @@
         INVALID_PROMO_TYPE,                      // Không đúng loại giảm giá hoặc phụ thu
         PROMO_VALUE_OUT_OF_RANGE,                // Giá trị % phải từ 0-100
         PROMO_VALUE_FIXED,                       // Giá trị đã cố định, không được sửa
+        ITEM_CODE_REQUIRED
     }
 }
