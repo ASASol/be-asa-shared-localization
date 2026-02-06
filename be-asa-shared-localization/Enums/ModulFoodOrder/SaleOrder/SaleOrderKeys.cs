b5c02cb578b0fe3b0563892353f4321c7d5383cc
Huy Phạm <huypt@namviets.com>
2026-02-06 16:30:23 +0700

    fix

--- be-asa-shared-localization/Enums/ModulFoodOrder/SaleOrder/SaleOrderKeys.cs
+++ be-asa-shared-localization/Enums/ModulFoodOrder/SaleOrder/SaleOrderKeys.cs
@@ -1,6 +1,11 @@
 ﻿namespace be_asa_shared_localization.Enums.ModulFoodOrder.SaleOrder
 {
-    public class SaleOrderKeys
+    public enum SaleOrderKeys
     {
+        DATA_IS_LOCKED,                  // Dữ liệu đang bị khóa bởi người dùng khác
+        ORDER_INFO_MISSING,              // Đơn hàng thiếu thông tin Outlet/Phòng ban
+        SERVICE_NOT_FOUND,               // Dịch vụ không tồn tại
+        CURRENCY_PARAM_NOT_FOUND,        // Không tìm thấy thông số tiền tệ
+        PRODUCT_NOT_FOUND_LIST           // Không tìm thấy danh sách sản phẩm: {0}
     }
 }
