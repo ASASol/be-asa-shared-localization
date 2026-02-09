c85684eff12847c0a565dcd9a6445663029c3f7c
Huy Phạm <huypt@namviets.com>
2026-02-09 16:33:59 +0700

    fix

--- be-asa-shared-localization/Enums/ModulFoodOrder/SaleOrder/SaleOrderKeys.cs
+++ be-asa-shared-localization/Enums/ModulFoodOrder/SaleOrder/SaleOrderKeys.cs
@@ -6,6 +6,7 @@
         ORDER_INFO_MISSING,              // Đơn hàng thiếu thông tin Outlet/Phòng ban
         SERVICE_NOT_FOUND,               // Dịch vụ không tồn tại
         CURRENCY_PARAM_NOT_FOUND,        // Không tìm thấy thông số tiền tệ
-        PRODUCT_NOT_FOUND_LIST           // Không tìm thấy danh sách sản phẩm: {0}
+        PRODUCT_NOT_FOUND_LIST,          // Không tìm thấy danh sách sản phẩm: {0}
+        INVALID_LOCK_TYPE                // Đơn hàng đã bị khóa
     }
 }
