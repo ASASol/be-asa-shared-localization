6e5aab6ee1f555700f2139c8ba4dc04026f99a27
Huy Phạm <huypt110402@gmail.com>
2026-06-11 08:57:15 +0700

    update

--- be-asa-shared-localization/Enums/ModuleAuthen/Authentication/UserSettingKeys.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace be_asa_shared_localization.Enums.ModuleAuthen.Authentication
-{
-    public enum UserSettingKeys
-    {
-        NOT_FOUND,
-        ALREADY_EXISTS,
-        CANNOT_DELETE,
-    }
-}
---
+++ be-asa-shared-localization/Enums/ModuleAuthen/Setting/UserSettingKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace be_asa_shared_localization.Enums.ModuleAuthen.Setting
+{
+    public enum UserSettingKeys
+    {
+        NOT_FOUND,
+        ALREADY_EXISTS,
+        CANNOT_DELETE,
+        NOT_ALLOW_OVERRIDE,    // Cài đặt không cho phép user tự thay đổi
+        INVALID_OPTION_VALUE   // Truyền giá trị không nằm trong danh sách Options
+    }
+}
